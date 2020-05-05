79804f819226592062c9e3803fe4cebe54848dd7
Vipul Wairagade <vipulw@winjit.com>
2020-05-05 20:22:27 +0530

    Integrated rrweb for recording

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <WebViewer :path="`${publicPath}lib`" url="https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf"/>
+    <p>{{ count }}</p>
+    <button @click="stopRecord">replay</button>
+    <WebViewer
+      :path="`${publicPath}lib`"
+      url="https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf"
+    />
   </div>
 </template>
 
 <script>
-import WebViewer from './components/WebViewer.vue'
+import WebViewer from "./components/WebViewer.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    WebViewer
+    WebViewer,
   },
-  data () {
+  data() {
     return {
-      publicPath: process.env.BASE_URL
-    }
-  }
-}
+      publicPath: process.env.BASE_URL,
+      count: 0,
+    };
+  },
+  mounted() {
+    const self = this;
+    this.events = [];
+    this.stopper = rrweb.record({
+      emit(event) {
+        self.events.push(event);
+      },
+    });
+    // do some thing to update the view
+    this.timer = setInterval(() => {
+      this.count++;
+    }, 1000);
+  },
+  methods: {
+    stopRecord() {
+      this.stopper();
+      clearInterval(this.timer);
+      const replayer = new rrweb.Replayer(this.events);
+      replayer.play();
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/WebViewer.vue
+++ src/components/WebViewer.vue
@@ -1,32 +1,33 @@
 <template>
-  <div ref='viewer'></div>
+  <div ref="viewer"></div>
 </template>
 
 <script>
 /* eslint-disable */
-import WebViewer from '@pdftron/webviewer'
+import WebViewer from "@pdftron/webviewer";
 
 export default {
-  name: 'WebViewer',
+  name: "WebViewer",
   props: {
     path: String,
-    url: String
+    url: String,
   },
-  mounted: function () {
-        WebViewer({
-            path: this.path,
-            initialDoc: this.url, // replace with your own PDF file
-          }, this.$refs.viewer).then((instance) => {
-            // call apis here
-          });
-    }
-}
+  mounted() {
+    WebViewer(
+      {
+        path: this.path,
+        initialDoc: this.url, // replace with your own PDF file
+      },
+      this.$refs.viewer
+    ).then((instance) => {});
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 div {
-  width: 100%; 
+  width: 100%;
   height: 100vh;
 }
 </style>
